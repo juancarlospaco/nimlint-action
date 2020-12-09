6efd74e0841e6896da61b37b6fd89bff49754f54
Juan Carlos <JuanCarlospaco@gmail.com>
2020-12-09 20:22:12 -0300

    iniciar

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# nimlint-action
+# Nimlint-Action
+
+- [GitHub Action](https://github.com/features/actions) to auto-lint all your Nim source code files using [nimlint](https://github.com/nim-compiler-dev/nimlint).
+
+
+# Use
+
+```yaml
+on: push
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@master
+      - uses: jiro4989/setup-nim-action@v1
+      - uses: juancarlospaco/nimlint-action@main
+```
+
+
+# Options
+
+- `verbose` Optional, boolean, defaults to `false`.
+- `folders` Optional, comma separated list of folders, defaults to `"."`.
+
+
+Examples:
+
+```yml
+- uses: juancarlospaco/nimlint-action@main
+  with:
+    verbose: true
+    folders: "src,docs,examples"
+```
+
+
+```yml
+- uses: juancarlospaco/nimlint-action@main
+  with:
+    indent: false
+    folders: "src"
+```
+
+
+# Requisites
+
+- `jiro4989/setup-nim-action` to setup Nim.
+- `EndBug/add-and-commit` to commit all nimlint fixes back to the Git repo.
+
+
+# FAQ
+
+- Why not take care of setting up Nim by itself?.
+
+Because some people already do it with just Git or Gitnim or Choosenim or setup-nim-action.
+
+- Why not take care of commiting the files by itself?.
+
+Because some people already do it with `EndBug/add-and-commit` or `stefanzweifel/git-auto-commit-action` or `github-actions/auto-commit`.
---
+++ index.js
@@ -0,0 +1,65 @@
+'use strict';
+const fs = require('fs');
+const path = require('path');
+const core = require('@actions/core');
+const { exec } = require('child_process');
+
+
+const cfg = (key) => {
+  console.assert(key.length > 0);
+  const result = core.getInput(key).trim();
+  console.assert(result.length > 0);
+  return result;
+};
+
+
+const walk = (startPath, callback) => {
+  console.assert(startPath.length > 0);
+  var counter = 0;
+  if (!fs.existsSync(startPath)) {
+    return;
+  }
+  var files = fs.readdirSync(startPath);
+  for (var i = 0; i < files.length; i++) {
+    var filename = path.join(startPath, files[i]);
+    var stat = fs.lstatSync(filename);
+    if (stat.isDirectory()) {
+        walk(filename, callback);
+    } else {
+      if (filename.length > 0 && filename[0] != "." && filename.substr(filename.length - 4, filename.length) == ".nim") {
+        console.log(counter + "\t" + filename);
+        counter++;
+        callback(filename);
+      }
+    }
+  };
+};
+
+
+const walks = (currentValue, index) => {
+  console.log("\nfolder\t'" + currentValue + "'");
+  walk(currentValue, function (filename) {
+    const cmd = `nimlint --verbose:${ cfg('verbose') } --output:${ filename } --input:${ filename }`;
+    exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        core.setFailed(`${stderr} ${stdout} ${err}`);
+        return;
+      };
+    });
+  });
+};
+
+
+try {
+  const nimlintInstall = "nimble -y --noColor install https://github.com/nim-compiler-dev/nimlint.git";
+  exec(nimlintInstall, (err, stdout, stderr) => {
+    if (err) {
+      core.setFailed(`${stderr} ${stdout} ${err}`);
+      return;
+    } else {
+      cfg('folders').split(',').forEach(walks);
+    };
+  });
+} catch (error) {
+  core.setFailed(`${stderr} ${stdout} ${err}`);
+}
